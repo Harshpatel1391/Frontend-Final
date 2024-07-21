6c5e93eab30e51daa96ec29202acff917412645c
Harsh Patel <iamharsh1391@gmail.com>
2024-07-21 11:45:37 +0530

    Update apiCall.js

--- src/apis/apiCall.js
+++ src/apis/apiCall.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://backend-final-vji8.onrender.com'
+    //baseURL: 'https://backend-final-vji8.onrender.com',
+    baseURL : 'https://shop-app-backend-yzse.onrender.com'
 })
